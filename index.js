125b2863e9fc295e817de270ef93444524117f5e
Nikita <Nikita@example.com>
2023-04-26 23:06:23 +0530

    recent commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function changeMode(){
+    let mybody = document.body;
+    mybody.classList.toggle('docBody');
+
+    // let brandlinks= document.getElementsByClassName("brandLinks");
+    // brandlinks.style.property.color="white";
+}
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility='visible';
+    document.getElementByclassName('main-div').style.opacity='0.2';
+}
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility='hidden';
+    document.getElementByclassName('main-div').style.opacity='1';
+}
+
