8f1c5a00646e463bd4f44a809a929e18aaa2e59a
Nikita <Nikita@example.com>
2023-04-29 19:09:19 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function changeMode(){
     let mybody = document.body;
     mybody.classList.toggle('docBody');
-
-    // let brandlinks= document.getElementsByClassName("brandLinks");
-    // brandlinks.style.property.color="white";
+    let brandlinks= document.getElementsByClassName("brandLinks");
+    brandlinks.style.color="white";
 }
 function loadCoupon(){
     document.getElementById('coupon').style.visibility='visible';
     document.getElementByclassName('main-div').style.opacity='0.8';
+   
 }
 function closeCoupon(){
     document.getElementById('coupon').style.visibility='hidden';
